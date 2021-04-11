263e89b9affc237851659ce8277d1311a661d68b
Holly Heredia <hollyheredia@gmail.comgit commitgit statusgit config --global user.email hollyheredia@gmail.com>
2021-04-10 18:26:55 -0700

    This is my final code to the best of my current ability.

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,60 +8,55 @@ bank_csv = os.path.join("Resources","PyBank_Resource.csv")
 with open(bank_csv, 'r') as csvfile:
     bank_reader = csv.reader(bank_csv, delimiter=',')
 
-# Check that csv is reading properly by printing the header
-    csv_header = next(bank_reader)
-    #print(f"Header: {csv_header}")
+# Define/skip the header
+csv_header = next(bank_reader)
 
-# Find unique values of month/year and count them for final tally
-# Set total_months to 0
+# Definition block. Set variables and indexes to 0
 net_total = 0
 total_index = []
+difference_index = []
+profit_difference = 0
 greatest_gain = 0
 lamest_lost = 0
 greatest_month = 0
 lamest_month = 0
 
-#Everytime the loop encounters a month/year to add 1 to count
+# Start loop
 
 for row in bank_reader:
-    date = str(row[0])
-    total_index.append(date)
-    total_month = len(total_index)
+    #If row has a value
+    if row != 0:
+        # Look for date in 1st column, add to index, count index
+        date = str(row[0])
+        total_index.append(date)
+        total_month = len(total_index)
 
-# Net total of profits and loses
+        # Look at profit/losses column
+        profit_losses = int(row[1])
+        # Add profit/losses to a total
+        net_total = profit_losses + net_total
+        # Find the difference for greatest/lamest indications later
+        profit_difference = profit_losses - profit_difference
+        difference_index.append(profit_difference)
 
-# Take the last value and subtract the first value
+        # Reset profit_difference to remember for next loop
+        profit_difference = row[1]
 
-# Find first_value by taking the value that is in cell(1,1)
-#first_value = csv_reader(1,1)
+    # If the row in blank
+    elif row == 0:
+        # Reset profit difference
+        profit_difference = row[1]
 
-# find the last value of column 2 in the spreadsheet
-#last_value = 0 
-# Average of total changes of profits and loses
-# Take total number of profits and divide by total_months
-# Greatest increase in profits and loses over a time period
-# Find the biggest number in the profits and loses column
-   
-for row in bank_reader:
-    change_total = int(row[1])
-    net_total = change_total + net_total
-
-    if row[1] >= greatest_gain:
-        greatest_gain = int(row[1])
-        greatest_month = str(row[0])
-    elif row[1] <= lamest_lost:
-            lamest_lost = int(row[1])
-            lamest_month = str(row[0]) 
-    else:
-        greatest_gain = greatest_gain
-        greatest_month = greatest_month
-        lamest_lost = lamest_lost
-        lamest_month = lamest_month
-
+#calculations
 average_total = int(net_total/total_month)
-# Greatest decrease in profits and loses over a time period
-        
-# Export text file with results in addition to running it in the terminal
+
+# Finding the max and min for the profit/losses
+greatest_gain = difference_index.index(max(difference_index))
+lamest_lost = difference_index.index(min(difference_index))
+greatest_month = (date[int(greatest_gain)], max(difference_index))
+lamest_month = (date[int(lamest_lost)], min(difference_index))
+
+# Print statement in terminal
 print(f'Financial Analysis:')
 print(f'_____________________')
 print(f'Total months: {total_month}')
@@ -69,3 +64,17 @@ print(f'Total: $ {net_total}')
 print(f'Average Change: $ int{average_total}')
 print(f'Greatest Increase in Profits:  {greatest_month}   {greatest_gain}')
 print(f'Greatest Decrease in Profits:  {lamest_month}    {lamest_lost}')
+
+# Print statement to txt file
+# Text Path
+PyBank_Holly = os.path.join(PyBank_Solved.txt)
+
+# Open file to write
+with open(PyBank_Holly, 'w') as txtfile:
+    textfile.write(f'Financial Analysis:')
+    textfile.write(f'___________________')
+    textfile.write(f'Total months: {total_month}')
+    textfile.write(f'Total: $ {net_total}')
+    textfile.write(f'Average Change: $(int{average_total})')
+    textfile.write(f'Greatest Increase in Profits: {greatest_month}  {greatest_gain}')
+    textfile.write(f'Greatest Decrease in Profits: {lamest_month}    {lamest_lost}')
